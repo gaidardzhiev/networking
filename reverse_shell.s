5be7ad69e1e2c729780c266a18c0432cac9caa00
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-02-20 12:26:04 +0200

    reverse_shell.s

--- reverse_shell.s
+++ reverse_shell.s
@@ -1,25 +1,5 @@
 	.text
-	.syntax unified
-	.eabi_attribute	67, "2.09"
-	.eabi_attribute	6, 10
-	.eabi_attribute	7, 65
-	.eabi_attribute	8, 1
-	.eabi_attribute	9, 2
 	.fpu	neon
-	.eabi_attribute	34, 1
-	.eabi_attribute	15, 1
-	.eabi_attribute	16, 1
-	.eabi_attribute	17, 2
-	.eabi_attribute	20, 1
-	.eabi_attribute	21, 0
-	.eabi_attribute	23, 3
-	.eabi_attribute	24, 1
-	.eabi_attribute	25, 1
-	.eabi_attribute	38, 1
-	.eabi_attribute	18, 4
-	.eabi_attribute	26, 2
-	.eabi_attribute	14, 0
-	.file	"reverse_shell.c"
 	.globl	main
 	.p2align	2
 	.type	main,%function
